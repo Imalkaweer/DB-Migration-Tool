38a74f6287e121b83afe3cc13efc5ac4cfb07903
Imalka Weerasekara <imajavaweerasekara128@gmail.com>
2022-10-04 16:18:46 +0530

    Initial commit

---
+++ src/main/java/com/developer/db/migration/tool/poc/DbMigrationToolPocApplication.java
@@ -0,0 +1,13 @@
+package com.developer.db.migration.tool.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbMigrationToolPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbMigrationToolPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/developer/db/migration/tool/poc/DbMigrationToolPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.db.migration.tool.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbMigrationToolPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
